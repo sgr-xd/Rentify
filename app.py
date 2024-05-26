d8c71c853df5c3e95d0ef634ded9990b803b39b6
sgr-xd <nerdzspot@gmail.com>
2024-05-27 01:18:52 +0530

    deployment_check

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def like_property():
     
     return redirect(url_for('buyer_dashboard'))
 
-port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
     app.debug = False
-    app.run(host='127.0.0.1', port=port)
+    app.run()
